e2f60e3333cabab01a1c8fa9689623da9230f108
SasaJ <jugurdzija.sasa@gmail.com>
2016-06-15 22:38:52 +0200

    initial commit

---
+++ app/src/main/java/com/sasaj/graphics/drawingapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.sasaj.graphics.drawingapp;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar_layout);
+    }
+}
