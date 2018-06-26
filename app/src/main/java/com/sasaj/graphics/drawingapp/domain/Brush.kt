0d5dc8d2e78df30a62752ec08694d6ef855da33d
sjugurdzija <jugurdzija.sasa@gmail.com>
2018-06-26 23:49:58 +0200

    Draw Background in BrushSample
    *Remove bg_contour drawable and references.
    *Add createCheckerboard method in BrushSample to create bg drawing paint object.
    *Draw bg inside onDraw method.

---
+++ app/src/main/java/com/sasaj/graphics/drawingapp/domain/Brush.kt
@@ -0,0 +1,4 @@
+package com.sasaj.graphics.drawingapp.domain
+
+class Brush(val size: Int, val blur: Float, val color: Int){
+}
--- paintselector/src/main/java/com/sasaj/graphics/paintselector/BrushSample.java
+++ paintselector/src/main/java/com/sasaj/graphics/paintselector/BrushSample.java
@@ -1,9 +1,12 @@
 package com.sasaj.graphics.paintselector;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +20,7 @@ public class BrushSample extends View {
     private static final String TAG = "BrushSample";
     Path path;
     Paint paint;
+    private Paint bgPaint;
 
     public BrushSample(Context context) {
         super(context);
@@ -35,6 +39,7 @@ public class BrushSample extends View {
 
     private void init() {
         setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
     }
 
 
@@ -46,33 +51,54 @@ public class BrushSample extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int width = MeasureSpec.getSize(widthMeasureSpec);
-        int height = width/4;
-        setMeasuredDimension(width,height);
+        int height = width / 4;
+        setMeasuredDimension(width, height);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         path = createPath(w, h);
+        bgPaint = createCheckerBoard(h / 6);
     }
 
     private Path createPath(int w, int h) {
         Path path = new Path();
-        float widthUnit = w/15;
-        float heightUnit = h/6;
+        float widthUnit = w / 15;
+        float heightUnit = h / 6;
 
         path.moveTo(widthUnit * 2, heightUnit * 3);
-        path.quadTo(widthUnit*4, heightUnit*2, widthUnit*6,  heightUnit*3);
-        path.quadTo( widthUnit*8, heightUnit*4, widthUnit*10,  heightUnit*3);
-        path.quadTo(widthUnit*11.5f,  heightUnit*2.25f, widthUnit*13,  heightUnit*2.75f);
+        path.quadTo(widthUnit * 4, heightUnit * 2, widthUnit * 6, heightUnit * 3);
+        path.quadTo(widthUnit * 8, heightUnit * 4, widthUnit * 10, heightUnit * 3);
+        path.quadTo(widthUnit * 11.5f, heightUnit * 2.25f, widthUnit * 13, heightUnit * 2.75f);
         return path;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if(path != null && paint != null){
+        if (bgPaint != null)
+            canvas.drawRect(0, 0, getWidth(), getHeight(), bgPaint);
+        if (path != null && paint != null) {
             canvas.drawPath(path, paint);
         }
+    }
+
+    private Paint createCheckerBoard(int pixelSize) {
+        Bitmap bitmap = Bitmap.createBitmap(pixelSize * 2, pixelSize * 2, Bitmap.Config.ARGB_8888);
+
+        Paint fill = new Paint(Paint.ANTI_ALIAS_FLAG);
+        fill.setStyle(Paint.Style.FILL);
+        fill.setColor(0x22000000);
+
+        Canvas canvas = new Canvas(bitmap);
+        Rect rect = new Rect(0, 0, pixelSize, pixelSize);
+        canvas.drawRect(rect, fill);
+        rect.offset(pixelSize, pixelSize);
+        canvas.drawRect(rect, fill);
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setShader(new BitmapShader(bitmap, BitmapShader.TileMode.REPEAT, BitmapShader.TileMode.REPEAT));
+        return paint;
     }
 }
