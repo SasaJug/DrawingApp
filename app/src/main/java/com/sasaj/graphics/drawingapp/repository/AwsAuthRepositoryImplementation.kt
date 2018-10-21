55236ee78eea2df0ce7a34fa27fbde33ec535b2c
sjugurdzija <jugurdzija.sasa@gmail.com>
2018-10-22 00:57:22 +0200

    Implement AwsAuthRepository
    *Create AwsAuthRepositoryImplementation.
    *Implement initial functionality for chhecking current user.

---
+++ app/src/main/java/com/sasaj/graphics/drawingapp/repository/AwsAuthRepositoryImplementation.kt
@@ -0,0 +1,67 @@
+package com.sasaj.graphics.drawingapp.repository
+
+import android.content.Context
+import android.util.Log
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationContinuation
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ChallengeContinuation
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler
+import com.sasaj.graphics.drawingapp.cognito.CognitoHelper
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.Subject
+
+class AwsAuthRepositoryImplementation(val context: Context) {
+
+
+    companion object {
+        val TAG: String = AwsAuthRepositoryImplementation::class.java.simpleName
+    }
+
+    private var cognitoHelper: CognitoHelper? = null
+    private var authSubject: Subject<String> = BehaviorSubject.create<String>()
+    private var authenticationHandler: AuthenticationHandler? = object : AuthenticationHandler {
+        override fun onSuccess(userSession: CognitoUserSession, newDevice: CognitoDevice?) {
+            Log.i(TAG, "Login success: ")
+            authSubject.onNext("proceed")
+        }
+
+        override fun getAuthenticationDetails(authenticationContinuation: AuthenticationContinuation, userId: String) {
+            Log.i(TAG, "getAuthenticationDetails")
+            authSubject.onNext("getAuthDetails")
+        }
+
+        override fun getMFACode(continuation: MultiFactorAuthenticationContinuation) {
+            Log.i(TAG, "getMFACode")
+        }
+
+        override fun authenticationChallenge(continuation: ChallengeContinuation) {
+            Log.i(TAG, "gauthenticationChallenge")
+        }
+
+        override fun onFailure(exception: Exception) {
+            Log.e(TAG, "Login failure: ", exception)
+            authSubject.onError(exception)
+        }
+    }
+
+    init {
+        cognitoHelper = CognitoHelper(context)
+    }
+
+    fun isLoggedIn() {
+        val user = cognitoHelper?.userPool?.currentUser
+        val username = user?.userId
+        if (username != null) {
+            user.getSessionInBackground(authenticationHandler)
+        }
+    }
+
+
+    fun getAuthenticationSubject(): Subject<String> {
+        return authSubject
+    }
+
+
+}
--- app/src/main/java/com/sasaj/graphics/drawingapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/sasaj/graphics/drawingapp/ui/splash/SplashActivity.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v4.content.ContextCompat.startActivity
 import android.util.Log
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession
@@ -14,10 +15,16 @@ import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.Auth
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ChallengeContinuation
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler
+import com.sasaj.graphics.drawingapp.R.id.username
 import com.sasaj.graphics.drawingapp.cognito.CognitoHelper
+import com.sasaj.graphics.drawingapp.repository.AwsAuthRepositoryImplementation
 import com.sasaj.graphics.drawingapp.ui.authentication.LoginActivity
 import com.sasaj.graphics.drawingapp.ui.base.BaseActivity
 import com.sasaj.graphics.drawingapp.ui.main.MainActivity
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.Subject
 
 /**
  * Created by sjugurdzija on 4/22/2017
@@ -25,11 +32,36 @@ import com.sasaj.graphics.drawingapp.ui.main.MainActivity
 
 class SplashActivity : BaseActivity() {
 
-    private var username : String? = null
-    val cognitoHelper : CognitoHelper  =  CognitoHelper(this)
+    private var username: String? = null
+    private var repo: AwsAuthRepositoryImplementation? = null
+    private var authSubject: Subject<String>? = null
+    val cognitoHelper: CognitoHelper = CognitoHelper(this)
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        repo = AwsAuthRepositoryImplementation(this)
+        authSubject = repo!!.getAuthenticationSubject()
+
+        authSubject!!.observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { s: String ->
+                            if (s == "proceed") {
+                                Log.i(TAG, "Login success: ")
+                                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Log.i(TAG, "getAuthenticationDetails")
+                                val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }
+                        },
+                        { e -> {} },
+                        { {} }
+                )
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(this,
@@ -50,16 +82,16 @@ class SplashActivity : BaseActivity() {
 
 
     private fun findCurrent() {
-
-        val user = cognitoHelper.userPool.currentUser
-        username = user.userId
-        if (username != null) {
-            user.getSessionInBackground(authenticationHandler)
-        } else  {
-            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+        repo?.isLoggedIn()
+//        val user = cognitoHelper.userPool.currentUser
+//        username = user.userId
+//        if (username != null) {
+//            user.getSessionInBackground(authenticationHandler)
+//        } else {
+//            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
@@ -76,17 +108,13 @@ class SplashActivity : BaseActivity() {
         }
     }
 
-    private var authenticationHandler: AuthenticationHandler = object : AuthenticationHandler {
+    private
+    var authenticationHandler: AuthenticationHandler = object : AuthenticationHandler {
         override fun onSuccess(userSession: CognitoUserSession, newDevice: CognitoDevice?) {
-            Log.e(TAG, "Login success: ")
-            if (userSession.isValid && userSession.idToken.jwtToken.isNotEmpty()) {
-                Log.e(TAG, "onSuccess: " + userSession.idToken.jwtToken)
-                val intent = Intent(this@SplashActivity, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-            } else {
-                Log.e(TAG, "onSuccess: no session data")
-            }
+            Log.i(TAG, "Login success: ")
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
 
         override fun getAuthenticationDetails(authenticationContinuation: AuthenticationContinuation, userId: String) {
